202f2207644dd1039551981c0ad4df3c1f8a1470
Tim Kye <Timothy.Kye@nike.com>
2019-08-04 20:24:28 -0700

    update install:

--- init.sh
+++ init.sh
@@ -5,10 +5,13 @@ chsh -s `which zsh`
 
 # Install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
-brew install zsh zsh-completions
+brew install zsh zsh-completions wget tfenv
 git clone https://github.com/powerline/fonts.git
 (cd fonts && ./install.sh)
 (cd .oh-my-zsh/custom && git clone https://github.com/romkatv/powerlevel10k.git themes/powerlevel10k)
+
+# Install node/npm
+./dotfiles/install_nvm.sh
 
 # update rc
 ./dotfiles/link.sh
--- install_nvm.sh
+++ install_nvm.sh
@@ -1,9 +1,17 @@
 #!/usr/bin/env bash
 
 mkdir ~/.nvm
-wget -qO- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-nvm install 8 --lts
-nvm install 10 --lts
+export NVM_DIR="$HOME/.nvm" && (
+  git clone https://github.com/nvm-sh/nvm.git "$NVM_DIR"
+  cd "$NVM_DIR"
+  git checkout `git describe --abbrev=0 --tags --match "v[0-9]*" $(git rev-list --tags --max-count=1)`
+) && \. "$NVM_DIR/nvm.sh"
 
-nvm use 10
-nvm alias default 10
+
+bash -lic "nvm install 8 --lts"
+bash -lic "nvm install 10 --lts"
+bash -lic "nvm use 10"
+bash -lic "nvm alias default 10"
+
+# Install pnpm
+curl -L https://unpkg.com/@pnpm/self-installer | node
