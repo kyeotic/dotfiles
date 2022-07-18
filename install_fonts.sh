b0fa8a245b79bd3b7f4ff61f0b47fb839e1e02a9
Tim Kye <tim.kye@blueocean.ai>
2022-07-18 09:16:21 -0700

    fix: swap fira code install

--- install_fonts.sh
+++ install_fonts.sh
@@ -3,4 +3,5 @@
 brew tap homebrew/cask-fonts
 brew install font-hack-nerd-font
 brew install font-meslo-lg-nerd-font
-brew install --cask font-fira-code
+# brew install --cask font-fira-code
+brew install --cask font-fira-code-nerd-font
