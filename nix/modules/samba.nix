7ecb4df84bae814b0eb4aa83770c7374ddf941a4
Tim Kye <tim@kye.dev>
2025-05-16 00:51:16 -0700

    fix samba

--- nix/modules/samba.nix
+++ nix/modules/samba.nix
@@ -5,48 +5,45 @@
     cifs-utils
     samba
   ];
-  
-  fileSystems."/mnt/media" = {
-    device = "//kye-nas/media";
-    fsType = "cifs";
-    options = let
-      # this line prevents hanging on network split
-      automount_opts = "x-systemd.automount,noauto,x-systemd.idle-timeout=60,x-systemd.device-timeout=5s,x-systemd.mount-timeout=5s";
 
-    in ["${automount_opts},credentials=/etc/nixos/smb-secrets,uid=1000,gid=100"];
+  services.samba = {
+    enable = true;
+    smbd.enable = true;
+    settings.global.security = "user";
+    openFirewall = true;
   };
+
+  fileSystems = let cfg = name: {
+      device = "//192.168.0.11/${name}";
+      fsType = "cifs";
+      options = [
+      "credentials=/home/kyeotic/smb-secrets"
+      "rw"
+      "uid=1000"
+      "gid=100"
+      "vers=3.0" # adapt
+      "noauto"
+      "x-systemd.automount"
+      "x-systemd.requires=network-online.target"
+      "x-systemd.after=network-online.target"
+      "x-systemd.idle-timeout=60"
+      "x-systemd.device-timeout=5s"
+      "x-systemd.mount-timeout=5s"
+      ]; 
+
+    }; in {
+      "/mnt/media" = cfg "media";
+      "/mnt/nas" = cfg "nas";
+    };
+
 
   networking.firewall.extraCommands = ''iptables -t raw -A OUTPUT -p udp -m udp --dport 137 -j CT --helper netbios-ns'';
   services.gvfs.enable = true;
 
-  firewall = {
+  networking.firewall = {
     enable = true;
     allowedTCPPorts = [ 137 138 139 389 445 ];
     allowedUDPPorts = [ 137 138 139 389 445 ];
   };
-
-
-  # # List packages installed in system profile. To search, run:
-  # # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   cifs-utils
-  #   samba
-  # ];
-  # services.samba = {
-  #   enable = true;
-  #   securityType = "user";
-  # #  openFirewall = true;
-  #   extraConfig = ''
-  #     workgroup = WORKGROUP
-  #     client min protocol = CORE
-  #   '';
-  # };
-  # security.wrappers."mount.cifs" = {
-  #   program = "mount.cifs";
-  #   source = "${lib.getBin pkgs.cifs-utils}/bin/mount.cifs";
-  #   owner = "root";
-  #   group = "root";
-  #   setuid = true;
-  # };
 }
 
